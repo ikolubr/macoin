586cb169809eb740a15fbcc1a493f31350540aac
Igor Almeida <igoralmeida@Igors-MacBook-Pro.local>
2017-06-05 11:35:12 -0700

    first commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x23, 0x62, 0x39, 0x90, 0x84, 0x9f, 0x2e, 0x8f, 0x74, 0x46, 0x05, 0x54, 0x14, 0x31, 0x21, 0x11 } };
+}
